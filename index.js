eb9154a3a88aff460ad3ed5b9c281653f3dd0d65
segoto <js.gomez23@uniandes.edu.co>
2020-09-22 18:45:27 -0500

    ya falta es hacer hacer la renderización de los cards como debe ser y lo del shopping cart, esto sale en unas 2 horas más bien finolino remolino

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const url = "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
-
+let active = "Burguers";
+let burgerData = {};
+let saladData = {};
+let dessertsData = {};
+let tacosData = {};
+let drinksData = {};
 const burgersOnClick = () =>{
-
+    
 }
 
 const tacosOnClick = () =>{
@@ -24,7 +29,14 @@ const addToCart = () =>{
 
 }
 
+
+
 fetch(url).then((response)=> response.json())
     .then(response =>{
         console.log(response)
+        burgerData = response[0];
+        tacosData = response[1];
+        saladData = response[2];
+        dessertsData = response[3];
+        drinksData = response[4];
     })
